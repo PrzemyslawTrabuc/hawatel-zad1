20b58945d89ed5eb24f62b53dd7fcaf63443f8f8
Przemek <czemek17@gmail.com>
2022-10-24 20:40:08 +0200

    added api key

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -3,7 +3,13 @@ import { baseUrl } from "../api/gorest";
 export async function fetchData(endpoint: string, pageNumber?: number) {
   try {
     const response = await fetch(
-      `${baseUrl}${endpoint}?page=${pageNumber}&per_page=12`
+      `${baseUrl}${endpoint}?page=${pageNumber}&per_page=12`,
+      {
+        headers: {
+          Authorization:
+            "Bearer ee6c0a507d1b39d2ab806b9e94df904396c2a2785546fbe086da09659c81cf3e",
+        },
+      }
     );
     const data = await response.json();
     return data;
